9a18139adb12c34a19bc6e1e97175012a7bdf23e
Hachi8888 <h.nana.420@gmail.com>
2019-08-15 11:16:13 +0900

    Xlbのファイルのレイアウト完了

--- Card/XIBTableViewCell.swift
+++ Card/XIBTableViewCell.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class XIBTableViewCell: UITableViewCell {
-
+    
+    // 画像表示用
+    @IBOutlet weak var xibImage: UIImageView!
+    // 名前表示用ラベル
+    @IBOutlet weak var xibNameLabel: UILabel!
+    // 職業表示用ラベル
+    @IBOutlet weak var xibWorkLabel: UILabel!
+    // 出身地表示用ラベル
+    @IBOutlet weak var xibHomeLabel: UILabel!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
